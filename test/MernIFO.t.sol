77f37d5d7ec999bec6d71a25d7351e4ee4275819
Ginika Chinonso <ginika.chinonso.francis@gmail.com>
2023-03-30 19:36:46 +0100

    test: Implement test for MernIFO

--- test/MernIFO.t.sol
+++ test/MernIFO.t.sol
@@ -3,32 +3,49 @@ pragma solidity ^0.8.13;
 
 import "lib/forge-std/src/Test.sol";
 import "../src/MernIFO.sol";
+import "../src/Mern.sol";
+
 
 contract MernIFOTest is Test {
     MernIFO public ifo;
+    Mern public merntoken;
+
 
     function setUp() public {
         ifo = new MernIFO();
+        merntoken = new Mern("Mern", "MRN");
+        merntoken.mint(10000 ether);
     }
 
     function testCreateIFO() public {
-        ifo.createIFO();
+        ifo.createIFO(address(merntoken), 1000 ether, 5 minutes);
 
     }
 
-    function testStartIFO(uint256 x) public {
-        ifo.startIFO();
+    function testStartIFO() public {
+        testCreateIFO();
+        ifo.startIFO(1);
     }
 
-    function testContribute(uint256 x) public {
-        ifo.contribute(_id);
+    function testContribute() public {
+        testStartIFO();
+        console.log(address(this).balance);
+        ifo.contribute{value: 5 ether}(1);
+        console.log(address(this).balance);
     }
 
-    function testRecieveToken(uint256 x) public {
-        ifo.recieveToken(_id);
+    function testRecieveToken() public {
+        testContribute();
+        vm.warp(block.timestamp + 5 days);
+        ifo.recieveToken(1);
     }
 
-    function testWithdrawETH(uint256 x) public {
-        ifo.withdrawETH(_id);
+    function testWithdrawETH() public {
+        testContribute();
+        ifo.withdrawETH();
     }
+
+    fallback() external payable {}
+
+    receive() external payable {}
 }
